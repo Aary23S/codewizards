4ac92fa60b67aeb48cc872dda7a9d2449f5f4f1d
SAary001 <142585155+Aary23S@users.noreply.github.com>
2026-06-29 23:22:26 +0530

    Fix resource delete route export

--- server/routes/resource.routes.js
+++ server/routes/resource.routes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getResources, createResource } = require("../controllers/resource.controller");
+
+const {
+  getResources,
+  createResource,
+  deleteResource,
+} = require("../controllers/resource.controller");
+
 const { protect, requireRole } = require("../middleware/auth");
 
 router.get("/", getResources);
